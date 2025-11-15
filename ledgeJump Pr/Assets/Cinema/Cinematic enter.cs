0105de7726290a609070332883f2cf58662b32a8
Gu3M0Nst4 <97657445+Gu3M0Nst4@users.noreply.github.com>
2025-11-14 22:17:06 -0800

    Agregados techos faltantes y camara de anim

--- ledgeJump Pr/Assets/Cinema/Cinematic enter.cs
+++ ledgeJump Pr/Assets/Cinema/Cinematic enter.cs
@@ -4,11 +4,14 @@ public class Cinematicenter : MonoBehaviour
 {
     public GameObject thePlayer;
     public GameObject cutsceneCam;
+    public Animator animator;
 
     void OnTriggerEnter(Collider other)
     {
         cutsceneCam.SetActive(true);
         thePlayer.SetActive(false);
+        animator.Play("Camara Animation");
+        
     }
 
 }
